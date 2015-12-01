a33659899a107296d9c18e4ccd1fb19f498fe67d
Luc Patiny <luc@patiny.com>
2015-12-01 13:26:48 +0100

    Improve parser to deal with tailing space (adding trim)

---
+++ test/ir.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var XYParser = require('..');
+var fs = require('fs');
+
+
+describe.only('ir.asc', function () {
+    var filename="ir.asc";
+    var data=fs.readFileSync(__dirname + "/data/"+filename).toString();
+
+    var result=XYParser.parse(data);
+
+
+    it('Check array and length', function () {
+        result.should.be.instanceof(Array).and.have.length(3401);
+    });
+
+
+});
+
