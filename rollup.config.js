09a4529bd2a3203fb6aa6a17a093ac64e694eef9
Luc Patiny <luc@patiny.com>
2019-10-11 16:32:48 +0200

    fix: rollup config to include dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
     file: 'lib/index.js',
   },
+  plugins: [resolve(), commonjs()],
 };
